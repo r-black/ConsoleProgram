dc29d3aef2b3e54cacd729bfcae7e05d35b35abd
Roman Prudnikov <romaprudnickov@yandex.ru>
2020-06-09 11:33:56 +0300

    refactor

--- ConsoleProgram.cpp
+++ ConsoleProgram.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <fstream>
 #include <string>
 #include <iterator>
 #include <algorithm>
@@ -11,22 +12,32 @@ ConsoleProgram::ConsoleProgram(int argc, const char **argv)
     // Добавляем пункты меню
     m_desc.add_options()
             ("help,h", "prints information about the program and a description of the parameters")  // печатает информацию о программе и описание параметров
-            ("file,f",  po::value<std::string>(&m_inputFilePath)->composing(), "set input file path") // Входной файл
-            ("method,m", po::value<std::string>(&m_nameMethod)->composing(), "set method: 'checksum' or 'words'") // Метод
-            ("count,v", po::value<std::string>(&m_inputWord)->composing(), "prints the number of words in the specified file") // Вызов метода words - используется с параметром -v и словом, количество вхождений которого нужно определить
-            // ("checksum,m", po::value<std::string>()->value_name("checksum"), "method call checksum") // Вызов метода checksum - печатает 32-битную чексумму, рассчитанную по алгоритму checksum = word1 + word2 + … + wordN (word1..wordN – 32-хбитные слова, представляющие содержимое файла)
+            ("file,f",  po::value<std::string>(&m_inputFilePath)->composing(), "set input file path, the '-m' option is required") // Входной файл
+            ("method,m", po::value<std::string>(&m_nameMethod)->composing(), "set method: 'checksum' (prints a 32-bit checksum calculated by the checksum = word1 + word2 + ... + wordN algorithm (word1..wordN - 32-bit words representing the contents of a file)) or 'words' (count all words in input file), the '-f' option is required") // Метод
+            ("count,v", po::value<std::string>(&m_inputWord)->composing(), "count occurrences of a word, the '-f' and '-m words' options is required") // Вызов метода words - используется с параметром -v и словом, количество вхождений которого нужно определить
             ;
     po::store(po::parse_command_line(argc, argv, m_desc), m_vm);
     po::notify(m_vm);
 }
 
-size_t ConsoleProgram::wordsCount(std::ifstream& ifs, const std::string str)
+size_t ConsoleProgram::wordsCount(std::ifstream& ifs, const std::string str = {})
 {
-    size_t num = std::count_if(
-        std::istream_iterator<std::string>(ifs), 
-        std::istream_iterator<std::string>(), 
-        [&str](std::string s){ return s == str;}
-    );
+    size_t num = 0;
+    if(str.empty())
+    {
+        num = std::distance(
+            std::istream_iterator<std::string>(ifs), 
+            std::istream_iterator<std::string>()
+        ); 
+    }
+    else
+    {
+        num = std::count_if(
+            std::istream_iterator<std::string>(ifs), 
+            std::istream_iterator<std::string>(), 
+            [&str](std::string s){ return s == str;}
+        );
+    }
     return num;
 }
 
@@ -58,18 +69,22 @@ int ConsoleProgram::exec()
         if (m_nameMethod == "words" && m_vm.count("count"))
         {
             std::cout << wordsCount(file, m_inputWord) << std::endl; 
-        }        
+        }
+        else if (m_nameMethod == "words")
+        {
+            std::cout << wordsCount(file) << std::endl;
+        }
     }
     // Если есть запрос на справку
     else if (m_vm.count("help"))
     {
-        // То выводим описание меню
+        // выводим описание меню
         std::cout << m_desc << std::endl;
         return 1;
     }
     else
     {
-        // В противном случае предлагаем посмотреть справку
+        // предлагаем посмотреть справку
         std::cout << "Please, use -h option for information" << std::endl;
         return 1;
     }
