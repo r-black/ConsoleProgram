0fd44c73a49f81660c389bddde445aa6f17b984d
Roman Prudnikov <romaprudnickov@yandex.ru>
2020-06-10 10:16:48 +0300

    add exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <exception>
 
 #include "ConsoleProgram.h"
 
 int main(int argc, const char* argv[])
 {
-    ConsoleProgram app(argc, argv);
-    return app.exec();
+    try 
+    {
+        ConsoleProgram app(argc, argv);
+        return app.exec();
+    }
+    catch(std::exception& e) {
+        std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Exception of unknown type!\n";
+    return 1;
+    }
 }
